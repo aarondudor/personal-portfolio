77b86d6fa2a1063fc908373e68bd06fc9f998da4
aarondudor <aarondudor@gmail.com>
2023-07-12 10:43:18 -0500

    Changed order of projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,34 +18,6 @@ const Projects = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 px-16 py-3">
           <div
-            style={{ backgroundImage: `url(${OOH})` }}
-            className="shadow-indigo-500 shadow group container rounded-md flex justify-center items-center mx-auto text-center content-div h-36 w-3/4 sm:w-full sm:h-60"
-          >
-            <div className="opacity-0 group-hover:opacity-100 duration-100 hover:duration-100">
-              <span className="text-md sm:text-xl font-bold text-white tracking-wider">
-                Office Hour Hacks
-              </span>
-              <div className="pt-3 sm:pt-8 text-center">
-                <a href="">
-                  <button
-                    className="rounded-lg px-4 py-3 m-2 bg-white text-indigo-500 font-bold text-sm sm:text-lg hover:cursor-not-allowed disabled:opacity-50"
-                    disabled
-                  >
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/aarondudor/office-hours-queue"
-                  target="_blank"
-                >
-                  <button className="rounded-lg px-4 py-3 m-2 bg-white text-indigo-500 font-bold text-sm sm:text-lg hover:text-white hover:bg-indigo-500 hover:duration-100 duration-100">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
             style={{ backgroundImage: `url(${GTown})` }}
             className="shadow-indigo-500 shadow group container rounded-md flex justify-center items-center text-center mx-auto content-div h-36 w-3/4 sm:w-full sm:h-60"
           >
@@ -64,6 +36,34 @@ const Projects = () => {
                 </a>
                 <a
                   href="https://github.com/aarondudor/graphics-town"
+                  target="_blank"
+                >
+                  <button className="rounded-lg px-4 py-3 m-2 bg-white text-indigo-500 font-bold text-sm sm:text-lg hover:text-white hover:bg-indigo-500 hover:duration-100 duration-100">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${OOH})` }}
+            className="shadow-indigo-500 shadow group container rounded-md flex justify-center items-center mx-auto text-center content-div h-36 w-3/4 sm:w-full sm:h-60"
+          >
+            <div className="opacity-0 group-hover:opacity-100 duration-100 hover:duration-100">
+              <span className="text-md sm:text-xl font-bold text-white tracking-wider">
+                Office Hour Hacks
+              </span>
+              <div className="pt-3 sm:pt-8 text-center">
+                <a href="">
+                  <button
+                    className="rounded-lg px-4 py-3 m-2 bg-white text-indigo-500 font-bold text-sm sm:text-lg hover:cursor-not-allowed disabled:opacity-50"
+                    disabled
+                  >
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/aarondudor/office-hours-queue"
                   target="_blank"
                 >
                   <button className="rounded-lg px-4 py-3 m-2 bg-white text-indigo-500 font-bold text-sm sm:text-lg hover:text-white hover:bg-indigo-500 hover:duration-100 duration-100">
